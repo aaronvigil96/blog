69e0bd42c3b7922d64e6bad17ae032b98b64be01
aaronvigil96 <aaronvigil96@gmail.com>
2025-01-16 21:48:49 -0300

    first commit

---
+++ src/config/envs.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+    PORT:number;
+    POSTGRES_USER:string;
+    POSTGRES_PASSWORD:string;
+    POSTGRES_DB:string;
+    DATABASE_URL:string;
+}
+
+const envsSchema = joi.object({
+    PORT: joi.number().required()
+})
+.unknown(true);
+
+const {error, value} = envsSchema.validate(process.env);
+
+if( error ) throw new Error(`Config validation error: ${error.message}`);
+
+const envVars:EnvVars = value;
+
+export const envs = {
+    port: envVars.PORT,
+    postgres_user: envVars.POSTGRES_USER,
+    postgres_password: envVars.POSTGRES_PASSWORD,
+    postgres_db: envVars.POSTGRES_DB,
+    database_url: envVars.DATABASE_URL
+}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  )
+
+  await app.listen(envs.port ?? 3000);
+  console.log(`Server running in port: ${envs.port}`);
+}
+bootstrap();
